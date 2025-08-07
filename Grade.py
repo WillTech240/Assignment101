6d4b36c30a48297d506e359014a7a580c22863ec
WILFRED <wilfredosei73@gmail.com>
2025-08-07 20:53:31 +0000

    Grade1.py

--- Grade.py
+++ Grade.py
@@ -1,14 +1,22 @@
-grade1 = int(input("Enter Your Scores Here"))
+# Get input grades for each subject
+grade1 = int(input("English: "))
+grade2 = int(input("Mathematics: "))
+grade3 = int(input("Integrated Science: "))
+grade4 = int(input("Social Studies: "))
 
-#English grade (grade1)
-if grade1 <= 100 and grade1 >= 90:
-    print("A")
+# Define a function to get grade based on score
+def get_grade(score):
+    if 90 <= score <= 100:
+        return "A"
+    elif 80 <= score <= 89:
+        return "B"
+    elif 70 <= score <= 79:
+        return "C"
+    else:
+        return "F9"
 
-elif grade1 <=89 and grade1 >= 80:
-    print("B")
-
-elif grade1 >= 70 and grade1 <= 79:
-    print("C")
-
-elif grade1 < 70:
-    print("F9")
+# Print grades
+print("English Grade:", get_grade(grade1))
+print("Mathematics Grade:", get_grade(grade2))
+print("Integrated Science Grade:", get_grade(grade3))
+print("Social Studies Grade:", get_grade(grade4))
